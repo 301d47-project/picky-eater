dc5f4759fbcf6ad8258558946118b5c264fdf7f7
Jon Rice <revyolution1120@gmail.com>
2019-05-28 13:51:34 -0700

    Working on search suggestion

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 app.get('/', loadHomePage);
 app.get('/about', loadAboutPage);
 app.post('/results', performSearch);
+app.post('/suggestion', searchSuggestion);
 
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
@@ -72,11 +73,28 @@ function loadAboutPage(request, response) {
 
 }
 
+function searchSuggestion(request, response) {
+    const apiSuggestion = require("request");
+    const querySuggestion = request.body.expression;
+
+    const suggestion = {
+        method: 'GET',
+        url: `http://platform.fatsecret.com/rest/server.api?method=foods.autocomplete&expression=${querySuggestion}&format=json`,
+        headers:{'content-type':'application/json'},
+        auth: {
+            bearer: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1NTkwMDAyOTgsImV4cCI6MTU1OTA4NjY5OCwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjpbImh0dHBzOi8vb2F1dGguZmF0c2VjcmV0LmNvbS9yZXNvdXJjZXMiLCJiYXNpYyJdLCJjbGllbnRfaWQiOiIwYzVhYTFmODkzMGQ0YzRkYTRkODU3N2MzYmI0ZTZkOSIsInNjb3BlIjpbImJhc2ljIl19.qHjgol9X7H8ojLcj3IXfsLCG5Or5e7DeCLjKFOIAEvJRMJ8FG5t23lmDAq8EALVAHaEQ0FffQajlhzkJRjpVgOq5HFNWlYff56wmtkT3LcMKLL_iOCidHVolfzSVHqGdv_QRe-iqVCq4SJx1mp99QL-l1oPLqvR2g5-m5UTYMo1mPmPZUV1BpSuZqfTeriaC_8z3fWzNz9K903gldqHaMpIF7g9DKP1j29__6-NIDadlACbZglqGniICz9T8w66MO35jRj0Wpz5yiHRahXQpP-3qLUByjV0drVXnBcKqcplzNQh9CN_xwzXNt-W3BtTrbiDE0ELX99AFNYgJ1vqW6A'
+        }
+    }
+    apiSuggestion(suggestion, function(error, suggestionApi, body) {
+        if (error) throw new Error(error);
+    })
+}
+
 function performSearch(request, response) {
 
     const apiRequest = require("request");
     const query = request.body.expression;
-    console.log(query);
+    // console.log(query);
 
     const options = {
         method: 'POST',
@@ -86,7 +104,7 @@ function performSearch(request, response) {
             bearer: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1NTkwMDAyOTgsImV4cCI6MTU1OTA4NjY5OCwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjpbImh0dHBzOi8vb2F1dGguZmF0c2VjcmV0LmNvbS9yZXNvdXJjZXMiLCJiYXNpYyJdLCJjbGllbnRfaWQiOiIwYzVhYTFmODkzMGQ0YzRkYTRkODU3N2MzYmI0ZTZkOSIsInNjb3BlIjpbImJhc2ljIl19.qHjgol9X7H8ojLcj3IXfsLCG5Or5e7DeCLjKFOIAEvJRMJ8FG5t23lmDAq8EALVAHaEQ0FffQajlhzkJRjpVgOq5HFNWlYff56wmtkT3LcMKLL_iOCidHVolfzSVHqGdv_QRe-iqVCq4SJx1mp99QL-l1oPLqvR2g5-m5UTYMo1mPmPZUV1BpSuZqfTeriaC_8z3fWzNz9K903gldqHaMpIF7g9DKP1j29__6-NIDadlACbZglqGniICz9T8w66MO35jRj0Wpz5yiHRahXQpP-3qLUByjV0drVXnBcKqcplzNQh9CN_xwzXNt-W3BtTrbiDE0ELX99AFNYgJ1vqW6A'
         }
     }
-    console.log(options.url);
+    // console.log(options.url);
     
     apiRequest(options, function(error, responseApi, body) {
         if (error) throw new Error(error);
