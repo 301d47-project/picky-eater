f522a59c601ae65f7850cab74b84bf587118eb8a
Jon Rice <revyolution1120@gmail.com>
2019-05-29 09:02:27 -0700

    Added new token into server.js

--- data/foodItems.sql
+++ data/foodItems.sql
@@ -2,7 +2,8 @@ DROP TABLE food;
 
 CREATE TABLE IF NOT EXISTS food (
     item VARCHAR(255),
-    description VARCHAR(500)
+    description VARCHAR(500),
+    food_id VARCHAR(255)
     );
 
     INSERT INTO food (item, description)
