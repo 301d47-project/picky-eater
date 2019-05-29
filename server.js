dbcbfbdd5820f76946f99c202774ea159cd7c56c
Alvian Joseph <alvianjoseph@gmail.com>
2019-05-28 18:04:27 -0700

    added charts to results page

--- server.js
+++ server.js
@@ -43,9 +43,6 @@ app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 app.get('/', loadHomePage);
 app.get('/about', loadAboutPage);
 app.post('/results', performSearch);
-app.post('/suggestion', searchSuggestion);
-app.get('/info', loadInfoPage);
-app.post('/recipes', getRecipe);
 
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
@@ -75,41 +72,20 @@ function loadAboutPage(request, response) {
 
 }
 
-function searchSuggestion(request, response) {
-    const apiSuggestion = require("request");
-    const querySuggestion = request.body.expression;
-
-    const suggestion = {
-        method: 'GET',
-        url: `http://platform.fatsecret.com/rest/server.api?method=foods.autocomplete&expression=${querySuggestion}&format=json`,
-        headers:{'content-type':'application/json'},
-        auth: {
-            bearer: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1NTkwMDAyOTgsImV4cCI6MTU1OTA4NjY5OCwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjpbImh0dHBzOi8vb2F1dGguZmF0c2VjcmV0LmNvbS9yZXNvdXJjZXMiLCJiYXNpYyJdLCJjbGllbnRfaWQiOiIwYzVhYTFmODkzMGQ0YzRkYTRkODU3N2MzYmI0ZTZkOSIsInNjb3BlIjpbImJhc2ljIl19.qHjgol9X7H8ojLcj3IXfsLCG5Or5e7DeCLjKFOIAEvJRMJ8FG5t23lmDAq8EALVAHaEQ0FffQajlhzkJRjpVgOq5HFNWlYff56wmtkT3LcMKLL_iOCidHVolfzSVHqGdv_QRe-iqVCq4SJx1mp99QL-l1oPLqvR2g5-m5UTYMo1mPmPZUV1BpSuZqfTeriaC_8z3fWzNz9K903gldqHaMpIF7g9DKP1j29__6-NIDadlACbZglqGniICz9T8w66MO35jRj0Wpz5yiHRahXQpP-3qLUByjV0drVXnBcKqcplzNQh9CN_xwzXNt-W3BtTrbiDE0ELX99AFNYgJ1vqW6A'
-        }
-    }
-    apiSuggestion(suggestion, function(error, suggestionApi, body) {
-        if (error) throw new Error(error);
-    })
-
-function loadInfoPage(request, response) {
-    response.render('pages/faqs')
-}
-
 function performSearch(request, response) {
 
     const apiRequest = require("request");
     const query = request.body.expression;
-    // console.log(query);
 
     const options = {
         method: 'POST',
         url: `https://platform.fatsecret.com/rest/server.api?method=foods.search&search_expression=${query}&format=json`,
         headers:{'content-type':'application/json'},
         auth: {
-            bearer: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1NTkwMDAyOTgsImV4cCI6MTU1OTA4NjY5OCwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjpbImh0dHBzOi8vb2F1dGguZmF0c2VjcmV0LmNvbS9yZXNvdXJjZXMiLCJiYXNpYyJdLCJjbGllbnRfaWQiOiIwYzVhYTFmODkzMGQ0YzRkYTRkODU3N2MzYmI0ZTZkOSIsInNjb3BlIjpbImJhc2ljIl19.qHjgol9X7H8ojLcj3IXfsLCG5Or5e7DeCLjKFOIAEvJRMJ8FG5t23lmDAq8EALVAHaEQ0FffQajlhzkJRjpVgOq5HFNWlYff56wmtkT3LcMKLL_iOCidHVolfzSVHqGdv_QRe-iqVCq4SJx1mp99QL-l1oPLqvR2g5-m5UTYMo1mPmPZUV1BpSuZqfTeriaC_8z3fWzNz9K903gldqHaMpIF7g9DKP1j29__6-NIDadlACbZglqGniICz9T8w66MO35jRj0Wpz5yiHRahXQpP-3qLUByjV0drVXnBcKqcplzNQh9CN_xwzXNt-W3BtTrbiDE0ELX99AFNYgJ1vqW6A'
+            bearer: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1NTkwODc5NTUsImV4cCI6MTU1OTE3NDM1NSwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjpbImh0dHBzOi8vb2F1dGguZmF0c2VjcmV0LmNvbS9yZXNvdXJjZXMiLCJiYXNpYyJdLCJjbGllbnRfaWQiOiIwYzVhYTFmODkzMGQ0YzRkYTRkODU3N2MzYmI0ZTZkOSIsInNjb3BlIjpbImJhc2ljIl19.awtSpjPrdxQrXzrBgDx-Gj9L-cc7UTWQ5gTHNdwLuLG-UbeuqcuoK8NBjVeYGgHe-R8WbAHUS2hYpOdKUW5BkndNf-kHiBYYMvDjvMyANdUfHQQBiERAOnHnMXAm4AjO0MfKcMdsene4tucFqe5FYmvdiRdxWISPIMdin2Lo5g4wV8QOwWbUNUaB26FAo64bUtiz-8UYZxoV2FNFHKRGgrVfqvWJ7UuV8vCVgGS0X9BkIqRd6yy3ZslUkx-EAxtqOQJfLvlWPY-F-1GstHMQtggBbOFiJ12plW6L01L29ISoMf4feA4mXsqCHfiilus7i2NX7V-K4bGBZxhumVWy5Q',
+
         }
     }
-    // console.log(options.url);
     
     apiRequest(options, function(error, responseApi, body) {
         if (error) throw new Error(error);
@@ -118,29 +94,4 @@ function performSearch(request, response) {
         response.render('pages/results', {searchResults: foods})
     })};
 
-function getRecipe(request, response) {
     
-    const apiRequest = require("request");
-    const query = request.body.expression;
-    console.log('query', query);
-    
-
-    const options = {
-    method: 'POST',
-    url: `https://platform.fatsecret.com/rest/server.api?method=recipes.search&search_expression=${query}&format=json`,
-    headers:{'content-type':'application/json'},
-    auth: {
-        bearer: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1NTkwMDAyOTgsImV4cCI6MTU1OTA4NjY5OCwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjpbImh0dHBzOi8vb2F1dGguZmF0c2VjcmV0LmNvbS9yZXNvdXJjZXMiLCJiYXNpYyJdLCJjbGllbnRfaWQiOiIwYzVhYTFmODkzMGQ0YzRkYTRkODU3N2MzYmI0ZTZkOSIsInNjb3BlIjpbImJhc2ljIl19.qHjgol9X7H8ojLcj3IXfsLCG5Or5e7DeCLjKFOIAEvJRMJ8FG5t23lmDAq8EALVAHaEQ0FffQajlhzkJRjpVgOq5HFNWlYff56wmtkT3LcMKLL_iOCidHVolfzSVHqGdv_QRe-iqVCq4SJx1mp99QL-l1oPLqvR2g5-m5UTYMo1mPmPZUV1BpSuZqfTeriaC_8z3fWzNz9K903gldqHaMpIF7g9DKP1j29__6-NIDadlACbZglqGniICz9T8w66MO35jRj0Wpz5yiHRahXQpP-3qLUByjV0drVXnBcKqcplzNQh9CN_xwzXNt-W3BtTrbiDE0ELX99AFNYgJ1vqW6A'
-        }
-    }
-    // response.send(options.url);
-
-    apiRequest(options, function(error, responseApi, body) {
-    if (error) throw new Error(error);
-    console.log(body);
-    const data = JSON.parse(body);
-    // response.send(data);
-    const recipes = data.recipes.recipe;
-    // response.send(recipes);
-    response.render('pages/faqs', {recipeResults: recipes});
-})};
