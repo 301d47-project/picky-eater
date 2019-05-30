4dee1bc05043ac5ae5a9b4f977f76f234b3bad1c
Alvian Joseph <alvianjoseph@gmail.com>
2019-05-30 06:07:50 -0700

    set up db to take suggestions

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(express.static('./public'));
 
 
 // Database Setup
-// const client = new pg.Client(process.env.DATABASE_URL);
-// client.connect();
-// client.on('error', err => console.error(err));
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
 
 // View engine for server-side rendering template (EJS)
 app.set('view engine', 'ejs');
@@ -33,7 +33,7 @@ app.get('/', loadHomePage);
 app.get('/about', loadAboutPage);
 app.post('/results', performSearch);
 app.get('/cooking', loadInfoPage);
-app.post('/get-suggestions', searchSuggestion);
+app.post('/get-suggestions', searchSuggestionNew);
 app.post('/recipes', getRecipe);
 
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
@@ -64,43 +64,42 @@ function loadInfoPage(request, response) {
     response.render('pages/faqs')
 }
 
-function searchSuggestion(request, response) {
-    const query = request.body.expression;
-    let suggestions = [];
+function searchSuggestionNew(request,response) {
+const query = request.body.expression
+const SQL = `SELECT * FROM food`;
 
-   if (query.startsWith('spam') ) {
-        suggestions = ['spam', 'spam sandwich']
-    }   else if (query.startsWith('spag') ) {
-        suggestions = ['spaghetti', 'spagetti and meatballs']
-    }   else if (query.startsWith('span') ) {
-        suggestions = ['spanish rice']
-    }    else if (query.startsWith('spa') ) {
-        suggestions = ['spam', 'spam sandwich', 'spaghetti', 'spanish rice']
-    }   
+
+client.query(SQL)
+.then(result =>{
+    console.log(query);
+    let foods = result.rows[0].item.split(",")
+    let suggestions = []
+    foods.forEach(food => {
+       suggestions.push(food);
+    })
+
     response.send({'suggestions': { 'suggestion': suggestions}})
+})
 
 }
+
 function searchSuggestionOld(request, response) {
-    // response.send({ 'suggestions': { 'suggestion': [ 'chicken', 'chicken breast', 'chicken salad', 'chiken wings' ] }})
     const apiSuggestion = require("request");
     const querySuggestion = request.body.expression;
-
 
     const suggestion = {
         method: 'POST',
         url: `https://platform.fatsecret.com/rest/server.api?method=foods.autocomplete&expression=${querySuggestion}&format=json`,
         headers:{'content-type':'application/json'},
         auth: {
-            bearer: "eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1NTkxNTQwMDQsImV4cCI6MTU1OTI0MDQwNCwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjpbImh0dHBzOi8vb2F1dGguZmF0c2VjcmV0LmNvbS9yZXNvdXJjZXMiLCJiYXNpYyJdLCJjbGllbnRfaWQiOiJjMDYzNjY1NDkxZmY0MmQ4OWRhNDJkNzcxZTdkZjc3YiIsInNjb3BlIjpbImJhc2ljIl19.L3SkTfqhRwSwli6cxDSPp7ujGZfo7AwI_LEDZRHSmFdSkeRnQsR_fitRK7m1sR1u4Q6PYdTddUiO4OMeXWuZCaHaLAlSDwaQw2f9tE5z0zQcW65hoyATXKMsl5Dq7X4abp1J-S8z2OYxsklJ4c-lIKYztmz2jy9-7wPBQQHV445UnGgZUJLmngEpWIRC-sf5kbzMjKxiQlELzJoKtuZZJDsPe2TtiCiMEO_m7O3Y1O2-RJi6_VM81dApQpJLpHLk-d4k2SPZrMuf_jYoZyrm3nncp3ix67vDp6fZPUK66WxCoo4_hBIHIqEQTFnSDlnF-M6GeN-cchiCfirtaRDvwQ"
+            bearer:'eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1NTkyMjA5MzcsImV4cCI6MTU1OTMwNzMzNywiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjpbImh0dHBzOi8vb2F1dGguZmF0c2VjcmV0LmNvbS9yZXNvdXJjZXMiLCJiYXNpYyJdLCJjbGllbnRfaWQiOiIwYzVhYTFmODkzMGQ0YzRkYTRkODU3N2MzYmI0ZTZkOSIsInNjb3BlIjpbImJhc2ljIl19.FEyZKXAwsqpSVj2uRRbr68iQWWoybK0VXQR45stCoq-41bMb1xBBur4qOh5sQFfZi4g7ohZq3TURO9FACkfuYYZE_OTU32oYPJevfN_TBQOxc8X7lIdMFYc_2Y6MCsOoDoEnFM9LlIyib_CJ2DkuR0Qmb30TcioKvyBPdZLijzd1ueqMTJAsKXNu9KUXBPILQ-ByDCQwYdIjDTzuQ6v_beFkaNZhtT1eyORlDYK7qUqHq_eEyTxIpmUpC65bTWhaUtY4rRyag0HSb5_26Kbm3M9uD8Y9ikmtIzO7JZO6EwEipyyvwSlkJzb-R3F3AO1TAGDtjg4hY3pDve7E1pZ19g'
 
         }
     }
 
     apiSuggestion(suggestion, function(error, suggestionApi, body) {
         if (error) throw new Error(error);
-        // const data = JSON.parse(body);
-        // const foods = data.foods.food.slice(0, 5);
-        // response.send({'suggestions': { 'suggestion': foods}})
+    
 })};
 
 
@@ -119,7 +118,7 @@ function performSearch(request, response) {
         url: `https://platform.fatsecret.com/rest/server.api?method=foods.search&search_expression=${query}&format=json`,
         headers:{'content-type':'application/json'},
         auth: {
-            bearer: "eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1NTkwODc5NTUsImV4cCI6MTU1OTE3NDM1NSwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjpbImh0dHBzOi8vb2F1dGguZmF0c2VjcmV0LmNvbS9yZXNvdXJjZXMiLCJiYXNpYyJdLCJjbGllbnRfaWQiOiIwYzVhYTFmODkzMGQ0YzRkYTRkODU3N2MzYmI0ZTZkOSIsInNjb3BlIjpbImJhc2ljIl19.awtSpjPrdxQrXzrBgDx-Gj9L-cc7UTWQ5gTHNdwLuLG-UbeuqcuoK8NBjVeYGgHe-R8WbAHUS2hYpOdKUW5BkndNf-kHiBYYMvDjvMyANdUfHQQBiERAOnHnMXAm4AjO0MfKcMdsene4tucFqe5FYmvdiRdxWISPIMdin2Lo5g4wV8QOwWbUNUaB26FAo64bUtiz-8UYZxoV2FNFHKRGgrVfqvWJ7UuV8vCVgGS0X9BkIqRd6yy3ZslUkx-EAxtqOQJfLvlWPY-F-1GstHMQtggBbOFiJ12plW6L01L29ISoMf4feA4mXsqCHfiilus7i2NX7V-K4bGBZxhumVWy5Q",
+            bearer: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1NTkyMjA5MzcsImV4cCI6MTU1OTMwNzMzNywiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjpbImh0dHBzOi8vb2F1dGguZmF0c2VjcmV0LmNvbS9yZXNvdXJjZXMiLCJiYXNpYyJdLCJjbGllbnRfaWQiOiIwYzVhYTFmODkzMGQ0YzRkYTRkODU3N2MzYmI0ZTZkOSIsInNjb3BlIjpbImJhc2ljIl19.FEyZKXAwsqpSVj2uRRbr68iQWWoybK0VXQR45stCoq-41bMb1xBBur4qOh5sQFfZi4g7ohZq3TURO9FACkfuYYZE_OTU32oYPJevfN_TBQOxc8X7lIdMFYc_2Y6MCsOoDoEnFM9LlIyib_CJ2DkuR0Qmb30TcioKvyBPdZLijzd1ueqMTJAsKXNu9KUXBPILQ-ByDCQwYdIjDTzuQ6v_beFkaNZhtT1eyORlDYK7qUqHq_eEyTxIpmUpC65bTWhaUtY4rRyag0HSb5_26Kbm3M9uD8Y9ikmtIzO7JZO6EwEipyyvwSlkJzb-R3F3AO1TAGDtjg4hY3pDve7E1pZ19g'
 
         }
     }
@@ -145,7 +144,7 @@ function getRecipe(request, response) {
     url: `https://platform.fatsecret.com/rest/server.api?method=recipes.search&search_expression=${query}&format=json`,
     headers:{'content-type':'application/json'},
     auth: {
-        bearer: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1NTkwODc5NTUsImV4cCI6MTU1OTE3NDM1NSwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjpbImh0dHBzOi8vb2F1dGguZmF0c2VjcmV0LmNvbS9yZXNvdXJjZXMiLCJiYXNpYyJdLCJjbGllbnRfaWQiOiIwYzVhYTFmODkzMGQ0YzRkYTRkODU3N2MzYmI0ZTZkOSIsInNjb3BlIjpbImJhc2ljIl19.awtSpjPrdxQrXzrBgDx-Gj9L-cc7UTWQ5gTHNdwLuLG-UbeuqcuoK8NBjVeYGgHe-R8WbAHUS2hYpOdKUW5BkndNf-kHiBYYMvDjvMyANdUfHQQBiERAOnHnMXAm4AjO0MfKcMdsene4tucFqe5FYmvdiRdxWISPIMdin2Lo5g4wV8QOwWbUNUaB26FAo64bUtiz-8UYZxoV2FNFHKRGgrVfqvWJ7UuV8vCVgGS0X9BkIqRd6yy3ZslUkx-EAxtqOQJfLvlWPY-F-1GstHMQtggBbOFiJ12plW6L01L29ISoMf4feA4mXsqCHfiilus7i2NX7V-K4bGBZxhumVWy5Q'
+        bearer: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1NTkyMjA5MzcsImV4cCI6MTU1OTMwNzMzNywiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjpbImh0dHBzOi8vb2F1dGguZmF0c2VjcmV0LmNvbS9yZXNvdXJjZXMiLCJiYXNpYyJdLCJjbGllbnRfaWQiOiIwYzVhYTFmODkzMGQ0YzRkYTRkODU3N2MzYmI0ZTZkOSIsInNjb3BlIjpbImJhc2ljIl19.FEyZKXAwsqpSVj2uRRbr68iQWWoybK0VXQR45stCoq-41bMb1xBBur4qOh5sQFfZi4g7ohZq3TURO9FACkfuYYZE_OTU32oYPJevfN_TBQOxc8X7lIdMFYc_2Y6MCsOoDoEnFM9LlIyib_CJ2DkuR0Qmb30TcioKvyBPdZLijzd1ueqMTJAsKXNu9KUXBPILQ-ByDCQwYdIjDTzuQ6v_beFkaNZhtT1eyORlDYK7qUqHq_eEyTxIpmUpC65bTWhaUtY4rRyag0HSb5_26Kbm3M9uD8Y9ikmtIzO7JZO6EwEipyyvwSlkJzb-R3F3AO1TAGDtjg4hY3pDve7E1pZ19g'    
         }
     }
     // response.send(options.url);
